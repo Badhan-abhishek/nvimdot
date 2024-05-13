d37936c19f1aa332e430216755e1674255efd7a9
badhan-abhishek <badhanabhishek3@gmail.com>
2024-05-13 11:22:24 +0530

    init lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -111,4 +111,19 @@ return {
   {
     'github/copilot.vim',
   },
+
+  {
+    'f-person/auto-dark-mode.nvim',
+    config = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option('background', 'dark')
+        vim.cmd 'colorscheme gruvbox'
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option('background', 'light')
+        vim.cmd 'colorscheme minischeme'
+      end,
+    },
+  },
 }
