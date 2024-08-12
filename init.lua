490e85be2ea91d75bd4d6513b5f16d2e617ae923
badhan-abhishek <badhanabhishek3@gmail.com>
2024-08-12 09:40:04 +0530

    add gK and no bg

--- init.lua
+++ init.lua
@@ -580,6 +580,8 @@ require('lazy').setup {
           --  For example, in C this would take you to the header
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('gK', vim.lsp.buf.signature_help, 'Signatiure Help')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
@@ -912,10 +914,15 @@ require('lazy').setup {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      -- vim.cmd.colorscheme 'habamax'
+      vim.cmd.colorscheme 'tokyonight-storm'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
+      require('tokyonight').setup {
+        on_colors = function(colors)
+          colors.bg = 'NONE'
+        end,
+      }
     end,
   },
 
